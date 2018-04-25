017d70857a4665b173bfbf4ed61a0d63bfec0143
Akshaykumar <cruseakshay@gmail.com>
2018-04-25 16:42:51 +0530

    added notes pyspark - Aggregate II

--- PySpark study/code snippets.py
+++ PySpark study/code snippets.py
@@ -112,3 +112,9 @@ flights.filter(flights.origin == "PDX").groupBy().min("distance").show()
 
 # Find the longest flight from SEA in terms of duration
 flights.filter(flights.origin == "SEA").groupBy().max("air_time").show()
+
+# Average duration of Delta flights
+flights.filter(flights.carrier == "DL").filter(flights.origin == "SEA").groupBy().avg("air_time").show()
+
+# Total hours in the air
+flights.withColumn("duration_hrs", flights.air_time/60).groupBy().sum("duration_hrs").show()
