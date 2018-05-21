ed77621d775c8de03e381ebfe251781eae4dd457
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-05-21 22:35:09 +0530

    ML-Python-started with Linear classification ch2

--- ML/python/Linear Classifiers in Python/code_snippets.py
+++ ML/python/Linear Classifiers in Python/code_snippets.py
@@ -60,3 +60,15 @@ for c in classifiers:
 plot_4_classifiers(X, y, classifiers)
 plt.show()
 
+# Changing the model coefficients
+# observe the effects of changing the coefficients of a linear classifer
+# Set the coefficients
+model.coef_ = np.array([[-1,1]])
+model.intercept_ = np.array([-3])
+
+# Plot the data and decision boundary
+plot_classifier(X,y,model)
+
+# Print the number of errors
+num_err = np.sum(y != model.predict(X))
+print("Number of errors:", num_err)
