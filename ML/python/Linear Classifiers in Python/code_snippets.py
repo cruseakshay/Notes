5b1ea1e7620b56c5b6d6b9add7155c175d5beb6a
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-05-21 22:22:09 +0530

    ML-Python- linear classifier ch1 done

--- ML/python/Linear Classifiers in Python/code_snippets.py
+++ ML/python/Linear Classifiers in Python/code_snippets.py
@@ -9,3 +9,54 @@ knn.fit(X_train, y_train)
 # Predict on the test features, print the results
 pred = knn.predict(X_test)[0]
 print("Prediction for test example 0:", pred)
+
+from sklearn import datasets
+digits = datasets.load_digits()
+Xtrain, Xtest, ytrain, ytest = train_test_split(digits.data, digits.target)
+
+# Apply logistic regression and print scores
+lr = LogisticRegression()
+lr.fit(Xtrain, ytrain)
+print(lr.score(Xtrain, ytrain))
+print(lr.score(Xtest, ytest))
+
+# Apply SVM and print scores
+svm = SVC()
+svm.fit(Xtrain, ytrain)
+print(svm.score(Xtrain, ytrain))
+print(svm.score(Xtest, ytest))
+
+# Sentiment analysis for movie reviews
+# X contains features based on the number of times words appear in the movie reviews, and y contains labels for whether the review sentiment is positive (+1) or negative (-1).
+# Instantiate logistic regression and train
+lr = LogisticRegression()
+lr.fit(X, y)
+
+# Predict sentiment for a glowing review
+review1 = "LOVED IT! This movie was amazing. Top 10 this year."
+review1_features = get_features(review1)
+print("Review:", review1)
+print("Probability of positive review:", lr.predict_proba(review1_features)[0,1])
+
+# Predict sentiment for a poor review
+review2 = "Total junk! I'll never watch a film by that director again, no matter how good the reviews."
+review2_features = get_features(review2)
+print("Review:", review2)
+print("Probability of positive review:", lr.predict_proba(review2_features)[0,1])
+
+# Visualizing decision boundaries
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.neighbors import KNeighborsClassifier
+
+# Define the classifiers
+classifiers = [LogisticRegression(), LinearSVC(), SVC(), KNeighborsClassifier()]
+
+# Fit the classifiers
+for c in classifiers:
+    c.fit(X, y)
+
+# Plot the classifiers
+plot_4_classifiers(X, y, classifiers)
+plt.show()
+
--- ML/python/Linear Classifiers in Python/slides/notes.md
+++ ML/python/Linear Classifiers in Python/slides/notes.md
@@ -3,3 +3,9 @@
 ## Applying logistic regression and SVM
 
 [Dataset used](http://ai.stanford.edu/~amaas/data/sentiment/)
+
+- Underfitting: model is too simple, low training accuracy
+- Overfitting: model is too complex, low test accuracy
+- Refer for Visualizing decision boundaries [Plot different SVM classifiers in the iris dataset](http://scikit-learn.org/stable/auto_examples/svm/plot_iris.html)
+
+## Loss functions
