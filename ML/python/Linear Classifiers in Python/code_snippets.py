ab1597477a8feaef86d38fc9caddfd3c61b23729
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-06-03 20:05:05 +0530

    ML - python - Done with linear classification

--- ML/python/Linear Classifiers in Python/code_snippets.py
+++ ML/python/Linear Classifiers in Python/code_snippets.py
@@ -277,3 +277,17 @@ print("Best CV accuracy", searcher.best_score_)
 print("Test accuracy of best grid search hypers:", searcher.score(X_test, y_test))
 
 # Using SGDClassifier
+# One advantage of SGDClassifier is that it's very fast - this would have taken a lot longer with LogisticRegression or LinearSVC.
+# We set random_state=0 for reproducibility 
+linear_classifier = SGDClassifier(random_state=0)
+
+# Instantiate the GridSearchCV object and run the search
+parameters = {'alpha':[0.00001, 0.0001, 0.001, 0.01, 0.1, 1], 
+             'loss':["hinge", "log"], 'penalty':["l1", "l2"]}
+searcher = GridSearchCV(linear_classifier, parameters, cv=10)
+searcher.fit(X_train, y_train)
+
+# Report the best parameters and the corresponding score
+print("Best CV params", searcher.best_params_)
+print("Best CV accuracy", searcher.best_score_)
+print("Test accuracy of best grid search hypers:", searcher.score(X_test, y_test))
