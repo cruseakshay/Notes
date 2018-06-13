2cbdd4bafd7cb034faad8b34f3f2b3fa9d7ac774
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-06-13 22:05:04 +0530

    ML - python - xgb: ch2 exercises

--- ML/python/Extreme Gradient Boosting with XGBoost/code_snippets.py
+++ ML/python/Extreme Gradient Boosting with XGBoost/code_snippets.py
@@ -96,3 +96,22 @@ preds = xg_reg.predict(X_test)
 # Compute the rmse: rmse
 rmse = np.sqrt(mean_squared_error(y_test, preds))
 print("RMSE: %f" % (rmse))
+
+# Linear base learners
+
+# Convert the training and testing sets into DMatrixes: DM_train, DM_test
+DM_train = xgb.DMatrix(X_train, y_train)
+DM_test =  xgb.DMatrix(X_test, y_test)
+
+# Create the parameter dictionary: params
+params = {"booster":"gblinear", "objective":"reg:linear"}
+
+# Train the model: xg_reg
+xg_reg = xgb.train(params = params, dtrain=DM_train, num_boost_round=5)
+
+# Predict the labels of the test set: preds
+preds = xg_reg.predict(DM_test)
+
+# Compute and print the RMSE
+rmse = np.sqrt(mean_squared_error(y_test,preds))
+print("RMSE: %f" % (rmse))
