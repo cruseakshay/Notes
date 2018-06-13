14728b5663b2501f126b46d3342dc1ee16bb32de
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-06-13 22:11:10 +0530

    ML - python - xgb: ch2 exercise

--- ML/python/Extreme Gradient Boosting with XGBoost/code_snippets.py
+++ ML/python/Extreme Gradient Boosting with XGBoost/code_snippets.py
@@ -115,3 +115,19 @@ preds = xg_reg.predict(DM_test)
 # Compute and print the RMSE
 rmse = np.sqrt(mean_squared_error(y_test,preds))
 print("RMSE: %f" % (rmse))
+
+# Evaluating model quality
+# Create the DMatrix: housing_dmatrix
+housing_dmatrix = xgb.DMatrix(data=X, label=y)
+
+# Create the parameter dictionary: params
+params = {"objective":"reg:linear", "max_depth":4}
+
+# Perform cross-validation: cv_results
+cv_results = xgb.cv(dtrain=housing_dmatrix, params=params, nfold=4, num_boost_round=5, metrics="mae", as_pandas=True, seed=123)
+
+# Print cv_results
+print(cv_results)
+
+# Extract and print final boosting round metric
+print((cv_results["test-mae-mean"]).tail(1))
