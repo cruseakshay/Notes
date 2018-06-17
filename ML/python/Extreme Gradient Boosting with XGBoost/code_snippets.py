3eca6405e1ccebd433f3476883aa0ba75f526965
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-06-17 16:37:59 +0530

    ML - python - xgb: Grid Search with XGBoost

--- ML/python/Extreme Gradient Boosting with XGBoost/code_snippets.py
+++ ML/python/Extreme Gradient Boosting with XGBoost/code_snippets.py
@@ -320,3 +320,28 @@ for curr_val in colsample_bytree_vals:
 
 # Print the resultant DataFrame
 print(pd.DataFrame(list(zip(colsample_bytree_vals, best_rmse)), columns=["colsample_bytree","best_rmse"]))
+
+# Grid Search with XGBoost
+# Create your housing DMatrix: housing_dmatrix
+housing_dmatrix = xgb.DMatrix(data=X, label=y)
+
+# Create the parameter grid: gbm_param_grid
+gbm_param_grid = {
+    'colsample_bytree': [0.3, 0.7],
+    'n_estimators': [50],
+    'max_depth': [2, 5]
+}
+
+# Instantiate the regressor: gbm
+gbm = xgb.XGBRegressor()
+
+# Perform grid search: grid_mse
+grid_mse = GridSearchCV(estimator=gbm, param_grid=gbm_param_grid, scoring="neg_mean_squared_error", cv=4, verbose=1)
+
+
+# Fit grid_mse to the data
+grid_mse.fit(X, y)
+
+# Print the best parameters and lowest RMSE
+print("Best parameters found: ", grid_mse.best_params_)
+print("Lowest RMSE found: ", np.sqrt(np.abs(grid_mse.best_score_)))
