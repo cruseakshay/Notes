7966bd755345df292f2f1ac5ac2876d02c2c8242
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-06-17 16:41:42 +0530

    ML - python - Random Search with XGBoost

--- ML/python/Extreme Gradient Boosting with XGBoost/code_snippets.py
+++ ML/python/Extreme Gradient Boosting with XGBoost/code_snippets.py
@@ -345,3 +345,23 @@ grid_mse.fit(X, y)
 # Print the best parameters and lowest RMSE
 print("Best parameters found: ", grid_mse.best_params_)
 print("Lowest RMSE found: ", np.sqrt(np.abs(grid_mse.best_score_)))
+
+# Random Search with XGBoost
+# Create the parameter grid: gbm_param_grid 
+gbm_param_grid = {
+    'n_estimators': [25],
+    'max_depth': range(2, 12)
+}
+
+# Instantiate the regressor: gbm
+gbm = xgb.XGBRegressor(n_estimators=10)
+
+# Perform random search: grid_mse
+randomized_mse = RandomizedSearchCV(estimator=gbm, scoring="neg_mean_squared_error", n_iter=5, cv=4, param_distributions=gbm_param_grid)
+
+# Fit randomized_mse to the data
+randomized_mse.fit(X, y)
+
+# Print the best parameters and lowest RMSE
+print("Best parameters found: ", randomized_mse.best_params_)
+print("Lowest RMSE found: ", np.sqrt(np.abs(randomized_mse.best_score_)))
