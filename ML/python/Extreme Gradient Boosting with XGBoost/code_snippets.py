1356810a72645c17db656cd122146be2647c24e3
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-06-17 16:09:01 +0530

    ML - python - xgb: Tuning max_depth

--- ML/python/Extreme Gradient Boosting with XGBoost/code_snippets.py
+++ ML/python/Extreme Gradient Boosting with XGBoost/code_snippets.py
@@ -266,3 +266,30 @@ for curr_val in eta_vals:
 
 # Print the resultant DataFrame
 print(pd.DataFrame(list(zip(eta_vals, best_rmse)), columns=["eta","best_rmse"]))
+
+# Tuning max_depth
+# Create your housing DMatrix
+housing_dmatrix = xgb.DMatrix(data=X,label=y)
+
+# Create the parameter dictionary
+params = {"objective":"reg:linear"}
+
+# Create list of max_depth values
+max_depths = [2, 5, 10, 20]
+best_rmse = []
+
+# Systematically vary the max_depth
+for curr_val in max_depths:
+
+    params["max_depth"] = curr_val
+    
+    # Perform cross-validation
+    cv_results = xgb.cv(dtrain=housing_dmatrix, params=params, seed=123, nfold=2, num_boost_round=10, early_stopping_rounds=5, metrics="rmse", as_pandas=True)
+    
+    
+    
+    # Append the final round rmse to best_rmse
+    best_rmse.append(cv_results["test-rmse-mean"].tail().values[-1])
+
+# Print the resultant DataFrame
+print(pd.DataFrame(list(zip(max_depths, best_rmse)),columns=["max_depth","best_rmse"]))
