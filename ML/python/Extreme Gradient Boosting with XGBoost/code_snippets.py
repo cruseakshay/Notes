5579210aec6462327975b92c167b55f623b2dfb6
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-06-18 22:53:00 +0530

    ML - python - Preprocessing within a pipeline

--- ML/python/Extreme Gradient Boosting with XGBoost/code_snippets.py
+++ ML/python/Extreme Gradient Boosting with XGBoost/code_snippets.py
@@ -432,3 +432,21 @@ print(df_encoded[:5,:])
 
 # Print the vocabulary
 print(dv.vocabulary_)
+
+# Preprocessing within a pipeline
+# Import necessary modules
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.pipeline import Pipeline
+
+# Fill LotFrontage missing values with 0
+X.LotFrontage = X.LotFrontage.fillna(0)
+
+# Setup the pipeline steps: steps
+steps = [("ohe_onestep", DictVectorizer(sparse=False)),
+         ("xgb_model", xgb.XGBRegressor())]
+
+# Create the pipeline: xgb_pipeline
+xgb_pipeline = Pipeline(steps)
+
+# Fit the pipeline
+xgb_pipeline.fit(X.to_dict("records"), y)
