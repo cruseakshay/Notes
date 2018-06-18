ef5bbd5fd0c8b26ba04c999be016e7e4d91c3920
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-06-18 23:13:42 +0530

    ML - python - Cross-validating XGBoost model

--- ML/python/Extreme Gradient Boosting with XGBoost/code_snippets.py
+++ ML/python/Extreme Gradient Boosting with XGBoost/code_snippets.py
@@ -450,3 +450,25 @@ xgb_pipeline = Pipeline(steps)
 
 # Fit the pipeline
 xgb_pipeline.fit(X.to_dict("records"), y)
+
+# Cross-validating your XGBoost model
+# Import necessary modules
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import cross_val_score
+
+# Fill LotFrontage missing values with 0
+X.LotFrontage = X.LotFrontage.fillna(0)
+
+# Setup the pipeline steps: steps
+steps = [("ohe_onestep", DictVectorizer(sparse=False)),
+         ("xgb_model", xgb.XGBRegressor(max_depth=2, objective="reg:linear"))]
+
+# Create the pipeline: xgb_pipeline
+xgb_pipeline = Pipeline(steps=steps)
+
+# Cross-validate the model
+cross_val_scores = cross_val_score(xgb_pipeline, X.to_dict("records"), y, scoring="neg_mean_squared_error",cv=10)
+
+# Print the 10-fold RMSE
+print("10-fold RMSE: ", np.mean(np.sqrt(np.abs(cross_val_scores))))
