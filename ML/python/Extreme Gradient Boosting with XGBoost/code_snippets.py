70d23d2a2feedda34da322a7c7b6099aad96f9f8
Akshaykumar <cruseakshay@gmail.com>
2018-06-19 14:25:59 +0530

    ML-python-Kidney disease case study III: Full pipeline

--- ML/python/Extreme Gradient Boosting with XGBoost/code_snippets.py
+++ ML/python/Extreme Gradient Boosting with XGBoost/code_snippets.py
@@ -516,3 +516,19 @@ numeric_categorical_union = FeatureUnion([
                                           ("num_mapper", numeric_imputation_mapper),
                                           ("cat_mapper", categorical_imputation_mapper)
                                          ])
+
+# Kidney disease case study III: Full pipeline
+# piece together all of the transforms along with an XGBClassifier to build the full pipeline
+# Create full pipeline
+pipeline = Pipeline([
+                     ("featureunion", numeric_categorical_union),
+                     ("dictifier", Dictifier()),
+                     ("vectorizer", DictVectorizer(sort=False)),
+                     ("clf", xgb.XGBClassifier())
+                    ])
+
+# Perform cross-validation
+cross_val_scores = cross_val_score(pipeline, kidney_data, y, scoring="roc_auc",cv=3)
+
+# Print avg. AUC
+print("3-fold AUC: ", np.mean(cross_val_scores))
