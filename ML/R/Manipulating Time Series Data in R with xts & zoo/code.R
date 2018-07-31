fb68f4ccbb95041d21e4c4ce9a89ee175a5eee32
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-07-31 21:33:18 +0530

    xts object

--- ML/R/Manipulating Time Series Data in R with xts & zoo/code.R
+++ ML/R/Manipulating Time Series Data in R with xts & zoo/code.R
@@ -1,2 +1,14 @@
 # Manipulating Time Series Data in R with xts & zoo
-# ch-1 
+# ch-1 Introduction to eXtensible Time Series, using xts and zoo for time series
+
+# Load xts
+library(xts)
+
+# View the structure of ex_matrix
+str(ex_matrix)
+
+# Extract the 3rd observation of the 2nd column of ex_matrix
+ex_matrix[3, 2]
+
+# Extract the 3rd observation of the 2nd column of core 
+core[3, 2]
