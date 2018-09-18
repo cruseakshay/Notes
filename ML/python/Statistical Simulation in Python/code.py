35e67c9b817ba2b1afb1a21f39000b475e3958cf
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-09-18 22:19:41 +0530

    ch2 ex3

--- ML/python/Statistical Simulation in Python/code.py
+++ ML/python/Statistical Simulation in Python/code.py
@@ -1,3 +1,14 @@
 # Statistical Simulation in Python
 # ch-1 Basics of randomness & simulation
 # Poisson random variable
+# Initialize seed and parameters
+np.random.seed(123) 
+lam, size_1, size_2 = 5, 3, 1000  
+
+# Draw samples & calculate absolute difference between lambda and sample mean
+samples_1 = np.random.poisson(lam, size_1)
+samples_2 = np.random.poisson(lam, size_2)
+answer_1 = abs(np.mean(samples_1)-lam)
+answer_2 = abs(np.mean(samples_2)-lam) 
+
+print("|Lambda - sample mean| with {} samples is {} and with {} samples is {}. ".format(size_1, answer_1, size_2, answer_2))
