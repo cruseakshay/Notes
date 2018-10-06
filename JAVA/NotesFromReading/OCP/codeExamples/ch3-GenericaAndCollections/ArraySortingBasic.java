6d60c17f9ecf27ec956400d46642b5746334f7b6
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-10-07 00:45:30 +0530

    Sorting arrays

---
+++ JAVA/NotesFromReading/OCP/codeExamples/ch3-GenericaAndCollections/ArraySortingBasic.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class ArraySortingBasic {
+    public static void main(String[] args) {
+        int[] intArray = {7, 4, 9, 4, 3};
+        System.out.println("Before sorting: ");
+        // using enhanced for loop
+        for (int var : intArray) {
+            System.out.print(var + " ");
+        }
+        System.out.println("\n");
+        Arrays.sort(intArray); // one of the overloaded methods
+        //Arrays.sort(intArray, 1, intArray.length-1); // second overriden method
+        System.out.println("After sorting: ");
+        // using enhanced for loop
+        for (int var : intArray) {
+            System.out.print(var + " ");
+        }
+    }    
+}
