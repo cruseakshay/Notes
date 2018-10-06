b2e92517d21bd778287ffe84d6f16381f2778b90
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-10-06 23:55:24 +0530

    Simulating the dice game

--- ML/python/Statistical Simulation in Python/code.py
+++ ML/python/Statistical Simulation in Python/code.py
@@ -42,3 +42,16 @@ else:
     answer = 'lose'
 
 print("The dice show {} and {}. You {}!".format(outcomes[0], outcomes[1], answer))
+
+# Simulating the dice game
+# Initialize model parameters & simulate dice throw
+die, probabilities, num_dice = [1,2,3,4,5,6], [1/6, 1/6, 1/6, 1/6, 1/6, 1/6], 2
+sims, wins = 100, 0
+
+for i in range(sims):
+    outcomes = np.random.choice(die, size=2, p=probabilities) 
+    # Increment `wins` by 1 if the dice show same number
+    if outcomes[0] == outcomes[1]: 
+        wins = wins + 1 
+
+print("In {} games, you win {} times".format(sims, wins))
