a7717c60eaf62f93bef132a4d95d5f03a8137fa8
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-10-09 00:03:38 +0530

    on Set

---
+++ JAVA/NotesFromReading/OCP/codeExamples/ch3-GenericaAndCollections/Exercise7.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+class Exercise7 {
+    public static void main(String[] args) {
+        Set<Number> numbers = new HashSet<>();
+        // add ele to the set: doesn't allow duplicates
+        numbers.add(new Integer(86));
+        numbers.add(75);
+        numbers.add(new Integer(86));
+        numbers.add(null); // important : null gets inserted
+        numbers.add(309L); // pay attension: this also gets inserted
+        Iterator iter = numbers.iterator();
+        while (iter.hasNext()) {
+            System.out.print(iter.next());
+        }
+
+    }
+}
