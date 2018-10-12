9d75c635e1732f3ee7d71317bb6c2fb41bf218cc
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-10-12 23:59:46 +0530

    working with forEach

---
+++ JAVA/NotesFromReading/OCP/codeExamples/ch3-GenericaAndCollections/Exercise24.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Exercise24 {
+    public static void main(String[] args) {
+        Set<String> s = new HashSet<>();
+        s.add("lion");
+        s.add("tiger");
+        s.add("bear");
+
+        // which of the following colpiles and prints an entire set ?
+    }
+}
