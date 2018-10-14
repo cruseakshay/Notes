78f84a1a38e5f21ea73cdf88b1fdba8daae79e45
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-10-14 22:52:49 +0530

    Supplier using StringBuilder

--- JAVA/NotesFromReading/OCP/codeExamples/ch4-FunctionalProgramming/ImpSupplier.java
+++ JAVA/NotesFromReading/OCP/codeExamples/ch4-FunctionalProgramming/ImpSupplier.java
@@ -3,6 +3,7 @@ import java.util.function.Supplier;
 
 class ImpSupplier {
     public static void main(String[] args) {
+        // Supplier using LocalDate
         Supplier<LocalDate> s1 = LocalDate::now; // implementaion one
         Supplier<LocalDate> s2 = () -> LocalDate.now(); // implementaion two
 
@@ -11,5 +12,12 @@ class ImpSupplier {
 
         System.out.println(d1);
         System.out.println(d2);
+
+        // Supplier using StringBuilder
+        Supplier<StringBuilder> s3 = StringBuilder::new; // using method reference
+        Supplier<StringBuilder> s4 = () -> new StringBuilder(); // using lambda 
+
+        System.out.println(s3.get());
+        System.out.println(s4.get());
     }
 }
