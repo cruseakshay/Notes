8a814b974d9c00b0b2e7da72d337c43f0a9e472c
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-10-16 00:00:08 +0530

    done

---
+++ JAVA/NotesFromReading/OCP/codeExamples/ch4-FunctionalProgramming/ImpConsumer.java
@@ -0,0 +1,14 @@
+import java.util.function.Consumer;
+
+class ImpConsumer {
+    public static void main(String[] args) {
+        // Consumer using method reference
+        Consumer<String> c1 = System.out::println;
+        
+        // Consumer using lambda
+        Consumer<String> c2 = x -> System.out.println(X);
+
+        c1.accept("ola");
+        c2.accept("yanna Consumer !");
+    }
+}
