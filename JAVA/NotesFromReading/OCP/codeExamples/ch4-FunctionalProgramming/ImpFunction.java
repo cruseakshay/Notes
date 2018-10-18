1bf7e19c92adfd9122f0e13343e6537207b06953
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-10-18 23:36:47 +0530

    done with Function

--- JAVA/NotesFromReading/OCP/codeExamples/ch4-FunctionalProgramming/ImpFunction.java
+++ JAVA/NotesFromReading/OCP/codeExamples/ch4-FunctionalProgramming/ImpFunction.java
@@ -1,5 +1,14 @@
+import java.util.function.Function;
+
 class ImpFunction {
     public static void main(String[] args) {
-        
+        Function<String, Integer> f1 = String::length; // using method reference.
+
+        Function<String, Integer> f2 = x -> x.length();  // using lambdas
+
+        // using Function Interface 
+        System.out.println(f1.apply("Akshay"));
+
+        System.out.println(f2.apply("Java Functional Prgramming"));
     }
 }
