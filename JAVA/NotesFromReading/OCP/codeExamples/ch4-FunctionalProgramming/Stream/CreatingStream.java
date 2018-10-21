4b850349bf71f6caa9c0927466d1ad81bf5e1b4a
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-10-21 11:54:21 +0530

    working with stream

---
+++ JAVA/NotesFromReading/OCP/codeExamples/ch4-FunctionalProgramming/Stream/CreatingStream.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+class CreatingStream {
+    // Stream interface is in java.util.stream package
+    public static void main(String[] args) {
+        // ways to create streams.
+
+        // 1. Empty Stream
+        Stream<String> empty = Stream.empty();
+
+        //2. Stream with single element.
+        Stream<Integer> singleEle = Stream.of(1);
+
+        //3. Stream from an array.
+        Stream<Integer> fromArray = Stream.of(1, 2, 3, 4);
+
+        //4. Converting from list to stream.
+        List<String> list = Arrays.asList("a", "b", "c");
+        Stream<String> fromList = list.stream();
+        Stream<String> fromListParallel = list.parallelStream(); // process ele in parallel.
+
+        //5. Creating infinite stream
+        Stream<Double> randoms = Stream.generate(Math::random); // generate stream of random numbers.
+        Stream<Integer> oddNUmbers = Stream.iterate(1, n -> n+2); // generate stream of odd numbers. Here 1 is seed/starting value
+        
+    }
+}
