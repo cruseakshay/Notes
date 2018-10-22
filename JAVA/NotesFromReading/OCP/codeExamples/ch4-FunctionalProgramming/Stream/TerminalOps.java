99521a2cff7dc166c3065d612d2110c597be0870
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-10-22 23:40:14 +0530

    started with collect()

--- JAVA/NotesFromReading/OCP/codeExamples/ch4-FunctionalProgramming/Stream/TerminalOps.java
+++ JAVA/NotesFromReading/OCP/codeExamples/ch4-FunctionalProgramming/Stream/TerminalOps.java
@@ -1,4 +1,5 @@
 import java.util.Optional;
+import java.util.TreeSet;
 import java.util.function.BinaryOperator;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
@@ -34,6 +35,7 @@ class TerminalOps {
 
         // 6. reduce()- combines a stream into an single object. has 3 varients.
         Stream<String> str = Stream.of("w","o", "l", "f");
+        // 1st varient
         String word = str.reduce("", (a,b) -> a+b); // identity, accumulator: start with an initial value and keep merging it with the next one.
         System.out.println(word);
 
@@ -43,9 +45,22 @@ class TerminalOps {
         Stream<Integer> oneEleStream = Stream.of(3);
         Stream<Integer> multiEleStream = Stream.of(3,5,6);
 
-        // observe the output
+        // 2nd varient: observe the output 
         emptyStream.reduce(ops).ifPresent(System.out::println); // if stream is empty, an empty Optional is returned.
         oneEleStream.reduce(ops).ifPresent(System.out::println); // if stream has one element it is returned.
         multiEleStream.reduce(ops).ifPresent(System.out::println); // if multiple ele then accumulator is applied to combine them.
+
+        //3rd varient: used when processing collections in parallel.
+        Stream<Integer> numStream = Stream.of(3,5,6);
+        System.out.println(numStream.reduce(1, ops, ops));// identity, accumulator, combiner
+
+        // 7. collect(): a mutable reduction-efficient. has 2 varients and very useful as it lets us get data out of streams and into another form.
+        // 1st varient : used when we want to code specifically how the collecting should work.
+        StringBuilder wordFormed = Stream.of("R","A","j","N","i","K","a","n","t").collect(StringBuilder::new, StringBuilder::append, StringBuilder::append); //supplier, BiConsumer-accumulator, BiConsumer-combiner. here logic is same in accumulator and combiner.
+        System.out.println(wordFormed);
+
+        // different logic in accumulator and combiner.
+        TreeSet<String> aSet = Stream.of("R","A","j","N","i","K","a","n","t").collect(TreeSet::new, TreeSet::add, TreeSet::addAll);
+        System.out.println(aSet);
     }
 }
