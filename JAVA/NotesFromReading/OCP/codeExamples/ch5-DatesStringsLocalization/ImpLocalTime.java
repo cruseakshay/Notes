fbb55685951f979a6ca78ed4ed6feab808a16277
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-10-26 23:58:57 +0530

    adding examples

--- JAVA/NotesFromReading/OCP/codeExamples/ch5-DatesStringsLocalization/ImpLocalTime.java
+++ JAVA/NotesFromReading/OCP/codeExamples/ch5-DatesStringsLocalization/ImpLocalTime.java
@@ -4,5 +4,7 @@ class ImpLocalTime{
     public static void main(String[] args) {
         // static methods
         System.out.println(LocalTime.of(6, 15)); // hour, minutes
+        System.out.println(LocalTime.of(6, 15, 30)); // hour, minutes, seconds
+        System.out.println(LocalTime.of(6, 15, 30, 200)); // hour, minutes, seconds, nanosec
     }
 }
