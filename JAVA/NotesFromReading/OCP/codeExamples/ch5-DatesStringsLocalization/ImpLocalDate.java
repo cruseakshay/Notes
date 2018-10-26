7efc31c51befb1f8400657c9aa48d5493564513d
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-10-26 23:55:58 +0530

    LocalDate practice

--- JAVA/NotesFromReading/OCP/codeExamples/ch5-DatesStringsLocalization/ImpLocalDate.java
+++ JAVA/NotesFromReading/OCP/codeExamples/ch5-DatesStringsLocalization/ImpLocalDate.java
@@ -1,3 +1,14 @@
+import java.time.LocalDate;
+import java.time.Month;
+
 class ImpLocalDate{
-    
+    public static void main(String[] args) {
+        // static methods
+        // 1. getting systems current date.
+        System.out.println(LocalDate.now()); 
+        // 2.a creating specific dates
+        System.out.println(LocalDate.of(2015, 1, 20)); // year, month, date
+        // 2.b creating specific dates
+        System.out.println(LocalDate.of(2015, Month.JANUARY, 20)); // year, Month, date. here Month is enum.
+    }
 }
