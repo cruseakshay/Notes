25b83a46a21035909ce77733c18ec14e36ebb8ee
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-10-26 23:57:58 +0530

    start with LocalTime

--- JAVA/NotesFromReading/OCP/codeExamples/ch5-DatesStringsLocalization/ImpLocalTime.java
+++ JAVA/NotesFromReading/OCP/codeExamples/ch5-DatesStringsLocalization/ImpLocalTime.java
@@ -1,3 +1,8 @@
+import java.time.LocalTime;
+
 class ImpLocalTime{
-    
+    public static void main(String[] args) {
+        // static methods
+        System.out.println(LocalTime.of(6, 15)); // hour, minutes
+    }
 }
