6553cca3d778e10e143df44809067d930a8ba14e
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-10-27 23:48:43 +0530

    added now() to LocalTime

--- JAVA/NotesFromReading/OCP/codeExamples/ch5-DatesStringsLocalization/ImpLocalTime.java
+++ JAVA/NotesFromReading/OCP/codeExamples/ch5-DatesStringsLocalization/ImpLocalTime.java
@@ -3,6 +3,7 @@ import java.time.LocalTime;
 class ImpLocalTime{
     public static void main(String[] args) {
         // static methods
+        System.out.println(LocalTime.now()); // prints sys curr time.
         System.out.println(LocalTime.of(6, 15)); // hour, minutes
         System.out.println(LocalTime.of(6, 15, 30)); // hour, minutes, seconds
         System.out.println(LocalTime.of(6, 15, 30, 200)); // hour, minutes, seconds, nanosec
