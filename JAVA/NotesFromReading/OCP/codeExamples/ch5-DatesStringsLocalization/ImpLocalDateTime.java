242ffdf4f4857e0bc6ab727ef6b7de8bf2a40db3
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-10-28 23:52:07 +0530

    adding methods to create date time objest

--- JAVA/NotesFromReading/OCP/codeExamples/ch5-DatesStringsLocalization/ImpLocalDateTime.java
+++ JAVA/NotesFromReading/OCP/codeExamples/ch5-DatesStringsLocalization/ImpLocalDateTime.java
@@ -1,5 +1,18 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+
 class ImpLocalDateTime{
     public static void main(String[] args) {
-        
+        System.out.println(LocalDateTime.now()); // curr date time of the sys.
+
+        // creating datetime object using date and time 
+        LocalDate date1 = LocalDate.of(2015, Month.JANUARY, 15);
+        LocalTime time1 = LocalTime.of(5, 50);
+
+        LocalDateTime dateAnTime = LocalDateTime.of(date1, time1);
+
+        //
     }
 }
