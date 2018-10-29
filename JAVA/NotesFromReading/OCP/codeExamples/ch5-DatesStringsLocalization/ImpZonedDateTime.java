502b25d0fec23c024b4dc20f4352ab485ea7ab1e
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-10-29 23:24:04 +0530

    now() for ZonedDateTime

--- JAVA/NotesFromReading/OCP/codeExamples/ch5-DatesStringsLocalization/ImpZonedDateTime.java
+++ JAVA/NotesFromReading/OCP/codeExamples/ch5-DatesStringsLocalization/ImpZonedDateTime.java
@@ -1,5 +1,7 @@
+import java.time.ZonedDateTime;
+
 class ImpZonedDateTime {
     public static void main(String[] args) {
-        
+        System.out.println(ZonedDateTime.now()); // get curr ZonedDateTime of sys.
     }
 }
