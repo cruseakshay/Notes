1dc73de40a1fed51d77565f5ad5002b27ffeab15
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-11-16 23:59:00 +0530

    rearrange code

--- JAVA/NotesFromReading/OCP/codeExamples/ch5-DatesStringsLocalization/ImpLocalDateTime.java
+++ JAVA/NotesFromReading/OCP/codeExamples/ch5-DatesStringsLocalization/ImpLocalDateTime.java
@@ -5,19 +5,6 @@ import java.time.Month;
 
 class ImpLocalDateTime{
     public static void main(String[] args) {
-        System.out.println(LocalDateTime.now()); // curr date time of the sys.
-
-        // creating datetime object using date and time 
-        LocalDate date1 = LocalDate.of(2015, Month.JANUARY, 15);
-        LocalTime time1 = LocalTime.of(5, 50);
-
-        LocalDateTime dateAnTime = LocalDateTime.of(date1, time1);
-        System.out.println(dateAnTime);
-
-        //other methods for creating datetime object
-        System.out.println(LocalDateTime.of(2015, 2, 24, 12, 11));; //year, int month, dayOfMonth, hour, minute
-        System.out.println(LocalDateTime.of(2015, Month.FEBRUARY, 24, 12, 11)); //year, Month month, dayOfMonth, hour, minute
-        
         /*
         Important method signatures:
 
@@ -35,5 +22,20 @@ class ImpLocalDateTime{
         
         public static LocalDateTime of(LocalDate date, LocalTime time)
         */
+        
+        System.out.println(LocalDateTime.now()); // curr date time of the sys.
+
+        // creating datetime object using date and time 
+        LocalDate date1 = LocalDate.of(2015, Month.JANUARY, 15);
+        LocalTime time1 = LocalTime.of(5, 50);
+
+        LocalDateTime dateAnTime = LocalDateTime.of(date1, time1);
+        System.out.println(dateAnTime);
+
+        //other methods for creating datetime object
+        System.out.println(LocalDateTime.of(2015, 2, 24, 12, 11));; //year, int month, dayOfMonth, hour, minute
+        System.out.println(LocalDateTime.of(2015, Month.FEBRUARY, 24, 12, 11)); //year, Month month, dayOfMonth, hour, minute
+        
+        
     }
 }
