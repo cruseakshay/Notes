f1360e770c2621088a12be3d6b338547b2d727f1
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-11-16 23:59:24 +0530

    rearrange code

--- JAVA/NotesFromReading/OCP/codeExamples/ch5-DatesStringsLocalization/ImpLocalDate.java
+++ JAVA/NotesFromReading/OCP/codeExamples/ch5-DatesStringsLocalization/ImpLocalDate.java
@@ -5,11 +5,14 @@ class ImpLocalDate{
     public static void main(String[] args) {
         // static methods
         // 1. getting systems current date.
-        System.out.println(LocalDate.now()); 
+        System.out.println(LocalDate.now());
+
         // 2.a creating specific dates
         System.out.println(LocalDate.of(2015, 1, 20)); // year, month, date
+        
         // 2.b creating specific dates
         System.out.println(LocalDate.of(2015, Month.JANUARY, 20)); // year, Month, date. here Month is enum.
+        
         System.out.println(LocalDate.of(2015, 1, 12)); // year, Month, date. here Month is int.
     }
 }
