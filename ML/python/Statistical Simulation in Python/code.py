3d6142cfca58ab7d940b27c7f05bd8b4675d2cbf
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-11-18 13:38:17 +0530

    simuating one lottery drawing

--- ML/python/Statistical Simulation in Python/code.py
+++ ML/python/Statistical Simulation in Python/code.py
@@ -57,3 +57,14 @@ for i in range(sims):
 print("In {} games, you win {} times".format(sims, wins))
 
 # Simulating one lottery drawing
+lottery_ticket_cost, num_tickets, grand_prize = 10, 1000, 1000000
+
+# Probability of winning
+chance_of_winning = 1/num_tickets
+
+# Code to simulate a single drawing of the lottery
+gains = [-lottery_ticket_cost, grand_prize-lottery_ticket_cost]
+probability = [1-chance_of_winning, chance_of_winning]
+outcome = np.random.choice(a=gains, size=1, p=probability, replace=True)
+
+print("Outcome of one drawing of the lottery is {}".format(outcome))
