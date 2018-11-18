73bfe1ac8995c8a54a35c74752c90c41839f2432
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-11-18 13:43:35 +0530

    Should we buy a lottery ? simulation

--- ML/python/Statistical Simulation in Python/code.py
+++ ML/python/Statistical Simulation in Python/code.py
@@ -68,3 +68,19 @@ probability = [1-chance_of_winning, chance_of_winning]
 outcome = np.random.choice(a=gains, size=1, p=probability, replace=True)
 
 print("Outcome of one drawing of the lottery is {}".format(outcome))
+
+# Should we buy?
+# Initialize size and simulate outcome
+lottery_ticket_cost, num_tickets, grand_prize = 10, 1000, 1000000
+chance_of_winning = 1/num_tickets
+size = 2000
+payoffs = [-lottery_ticket_cost, grand_prize-lottery_ticket_cost]
+probs = [1 - chance_of_winning, chance_of_winning]
+
+outcomes = np.random.choice(a=payoffs, size=size, p=probs, replace=True)
+
+# Mean of outcomes.
+answer = outcomes.mean()
+print("Average payoff from {} simulations = {}".format(size, answer))
+
+# Calculating a break-even lottery price
