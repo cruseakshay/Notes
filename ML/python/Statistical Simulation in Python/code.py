cc3d07df77b1fedeb20400fc56ad98aa1f5ed5ba
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-11-18 13:51:01 +0530

    break even lottery simulation

--- ML/python/Statistical Simulation in Python/code.py
+++ ML/python/Statistical Simulation in Python/code.py
@@ -84,3 +84,17 @@ answer = outcomes.mean()
 print("Average payoff from {} simulations = {}".format(size, answer))
 
 # Calculating a break-even lottery price
+# Initialize simulations and cost of ticket
+sims, lottery_ticket_cost = 3000, 0
+
+# Use a while loop to increment `lottery_ticket_cost` till average value of outcomes falls below zero
+while 1:
+    outcomes = np.random.choice([-lottery_ticket_cost, grand_prize-lottery_ticket_cost],
+                 size=sims, p=[1-chance_of_winning, chance_of_winning], replace=True)
+    if outcomes.mean() < 0:
+        break
+    else:
+        lottery_ticket_cost += 1
+answer = lottery_ticket_cost - 1
+
+print("The highest price at which it makes sense to buy the ticket is {}".format(answer))
