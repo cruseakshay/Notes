ebb1fe5247053335aec5adf6ccc01339b2ccd81e
Akshaykumar <cruseakshay@gmail.com>
2018-11-24 11:54:45 +0530

    method chaining in Period class

--- JAVA/NotesFromReading/OCP/codeExamples/ch5-DatesStringsLocalization/ImpPeriod.java
+++ JAVA/NotesFromReading/OCP/codeExamples/ch5-DatesStringsLocalization/ImpPeriod.java
@@ -16,13 +16,35 @@ class ImpPeriod{
         //Custom
         Period everyYearAndAWeek = Period.of(1, 0, 7); // Y, M, D
 
+        //toString of Period class:
+        System.out.println(annually);
+        System.out.println(monthly);
+        System.out.println(biWeekly);
+
         // Useage:
         LocalDate startPoint = LocalDate.of(2010, Month.JANUARY, 12);
 
         System.out.println("Added a Year: " + startPoint.plus(annually));
 
+        System.out.println("Added a Month: " + startPoint.plus(monthly));
+
+        System.out.println("Added 2 weeks: " + startPoint.plus(biWeekly));
+
+        System.out.println("Added 2 days: " + startPoint.plus(everyOtherDay));
+
+        System.out.println("Added aYearAndAWeek: " + startPoint.plus(everyYearAndAWeek));
 
         // You cannot chain methods when creating a Period. [IMP]
         // Only the last method is used in case of chaining because the Period of methods are static methods.
+
+        Period tricky = Period.ofYears(1).ofWeeks(1); // Note swiggly lines.
+        
+        System.out.println(tricky); // Note the output.
+
+        // Above code is equivalent to:
+        Period p = Period.ofYears(1);
+        p = Period.ofWeeks(1);
+        
+        System.out.println(p);
     }
 }
