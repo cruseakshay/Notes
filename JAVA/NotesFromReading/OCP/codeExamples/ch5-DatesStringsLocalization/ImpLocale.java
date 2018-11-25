766dad1faf5d639e24b78372e59f6bfd704932a2
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-11-25 13:04:11 +0530

    Locale constructors

--- JAVA/NotesFromReading/OCP/codeExamples/ch5-DatesStringsLocalization/ImpLocale.java
+++ JAVA/NotesFromReading/OCP/codeExamples/ch5-DatesStringsLocalization/ImpLocale.java
@@ -5,5 +5,17 @@ class ImpLocale {
         // getting current Locale of the user.
         Locale currLocale = Locale.getDefault();
         System.out.println(currLocale); // en_IN [lower case language code + underscore + uppercase country code]
+
+        // Note: Unerscore and country code are optional, valid Locale can be only a language.
+
+        // 3 Main ways of creating a Locale.
+
+        // 1. Using constants from the class.
+        System.out.println(Locale.GERMAN);  // Just the language.
+        System.out.println(Locale.GERMANY); // Note the difference. Lang + CCode
+
+        // 2, 3. Using constructors.
+        System.out.println(new Locale("fr")); // only Lang
+        System.out.println(new Locale("hi", "IN")); // Lang + CCode
     }
 }
