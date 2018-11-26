0cf9bd79609535d6374d1d3795429b382235cb7f
Akshaykumar <cruseakshay@gmail.com>
2018-11-26 20:26:35 +0530

    start with ChronoUnit

---
+++ JAVA/NotesFromReading/OCP/codeExamples/ch5-DatesStringsLocalization/ImpChronoUnit.java
@@ -0,0 +1,6 @@
+class ImpChronoUnit {
+    public static void main(String[] args) {
+        // using ChronoUnit to determine how far apart two Temporal values are.
+        // Temporal includes LocalDate, LocalTime, and so on.
+    }
+}
