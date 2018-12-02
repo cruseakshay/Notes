0094426edc83e5fd50b466f7371d7dfac41f6b37
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-12-03 00:03:06 +0530

    Static methods on DateTimeFormatter

--- JAVA/NotesFromReading/OCP/codeExamples/ch5-DatesStringsLocalization/ImpDateTimeFormatter.java
+++ JAVA/NotesFromReading/OCP/codeExamples/ch5-DatesStringsLocalization/ImpDateTimeFormatter.java
@@ -3,6 +3,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 
 class ImpDateTimeFormatter{
     public static void main(String[] args) {
@@ -17,5 +18,12 @@ class ImpDateTimeFormatter{
         System.out.println(time.format(DateTimeFormatter.ISO_LOCAL_TIME)); // Note Formatter used.
 
         System.out.println(dateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)); // Note Formatter used.
+
+        // Static methods.
+        DateTimeFormatter shortDateTime = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT); // Note methods used and FormatStyle.
+
+        System.out.println(shortDateTime.format(dateTime));
+        System.out.println(shortDateTime.format(date));
+        System.out.println(shortDateTime.format(time)); // UnsupportedTemporalTypeException: as Time cannot be formatted as Date.
     }
 }
