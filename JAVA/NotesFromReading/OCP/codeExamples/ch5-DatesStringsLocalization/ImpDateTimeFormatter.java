cd18600e31f76c359fbc13f3ff5a77e721d2eb89
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-12-02 23:55:52 +0530

    Using DateTimeFormatter

--- JAVA/NotesFromReading/OCP/codeExamples/ch5-DatesStringsLocalization/ImpDateTimeFormatter.java
+++ JAVA/NotesFromReading/OCP/codeExamples/ch5-DatesStringsLocalization/ImpDateTimeFormatter.java
@@ -1,3 +1,17 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+
 class ImpDateTimeFormatter{
-    // Formatting Dates and Times
+    public static void main(String[] args) {
+        // Formatting Dates and Times
+        LocalDate date = LocalDate.of(2018, Month.DECEMBER, 2);
+        LocalTime time = LocalTime.of(11, 52);
+        LocalDateTime dateTime = LocalDateTime.of(date, time);
+
+        // Using methods from object.
+        System.out.println(date.format(DateTimeFormatter.ISO_LOCAL_DATE)); // Note Formatter used.
+    }
 }
