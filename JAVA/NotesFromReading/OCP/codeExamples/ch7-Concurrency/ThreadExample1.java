d4a183dbbfc7f88e6efe97bbb52c72c8e38cf1f2
Akshaykumar <cruseakshay@gmail.com>
2018-12-11 12:17:11 +0530

    Creating Thread using Runnable instance.

--- JAVA/NotesFromReading/OCP/codeExamples/ch7-Concurrency/ThreadExample1.java
+++ JAVA/NotesFromReading/OCP/codeExamples/ch7-Concurrency/ThreadExample1.java
@@ -1,3 +1,13 @@
-public class ThreadExample1 {
-    
+public class ThreadExample1 implements Runnable{
+    @Override
+    public void run() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Record:: " + i);
+        }
+    }
+
+    // Creating Thread using Runnable instance.
+    public static void main(String[] args) {
+        (new Thread(new ThreadExample1())).start();
+    }
 }
