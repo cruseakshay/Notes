100d54d43a2285992b59587cb0eb53d59930d9a2
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-12-11 21:22:24 +0530

    Polling mechanism using Thread.sleep()

--- JAVA/NotesFromReading/OCP/codeExamples/ch7-Concurrency/ThreadSleep.java
+++ JAVA/NotesFromReading/OCP/codeExamples/ch7-Concurrency/ThreadSleep.java
@@ -1,3 +1,17 @@
 class ThreadSleep{
-    
+    private static int counter = 0;
+    public static void main(String[] args) throws InterruptedException{
+        new Thread(() -> {
+            for (int i = 0; i < 500; i++) {
+                // System.out.println(i);
+                ThreadSleep.counter++;
+            }
+        }).start();
+
+        while (ThreadSleep.counter < 100) {
+            System.out.println("Not reached yet !");
+            Thread.sleep(1000);
+        }
+        System.out.println("Reached !");
+    }
 }
