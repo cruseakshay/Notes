d7b2ff60470dcd5c6fe758ba82df6339825da4df
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-12-13 23:56:22 +0530

    exception handling

--- JAVA/NotesFromReading/OCP/codeExamples/ch7-Concurrency/CallableExample.java
+++ JAVA/NotesFromReading/OCP/codeExamples/ch7-Concurrency/CallableExample.java
@@ -3,7 +3,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 class ClassableExample{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, ExecutionException{
         ExecutorService service = null;
         try {
             service = Executors.newSingleThreadExecutor();
