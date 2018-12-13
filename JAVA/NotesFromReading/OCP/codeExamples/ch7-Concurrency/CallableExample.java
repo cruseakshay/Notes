10044cccf057693dba25a2216e7b29f544eb9aea
Akshaykumar Salunke <cruseakshay@gmail.com>
2018-12-13 23:58:19 +0530

    fix errors

--- JAVA/NotesFromReading/OCP/codeExamples/ch7-Concurrency/CallableExample.java
+++ JAVA/NotesFromReading/OCP/codeExamples/ch7-Concurrency/CallableExample.java
@@ -1,8 +1,9 @@
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-class ClassableExample{
+public class CallableExample{
     public static void main(String[] args) throws InterruptedException, ExecutionException{
         ExecutorService service = null;
         try {
