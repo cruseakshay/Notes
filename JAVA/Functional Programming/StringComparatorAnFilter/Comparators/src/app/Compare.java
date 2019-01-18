8645f71aaecd23d0323a22540da804d564c14ac0
Akshaykumar <cruseakshay@gmail.com>
2019-01-18 19:05:07 +0530

    min() and max() from comparison

--- JAVA/Functional Programming/StringComparatorAnFilter/Comparators/src/app/Compare.java
+++ JAVA/Functional Programming/StringComparatorAnFilter/Comparators/src/app/Compare.java
@@ -1,6 +1,7 @@
 package app;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -47,6 +48,29 @@ public class Compare {
 							(person1, person2) -> person2.ageDifference(person1)
 							).collect(Collectors.toList()));
 			System.out.println("//" + "END:AGE_DESCEND_OUTPUT");
+			
+			System.out.println("//" + "START:REVERSE_ORDER_OUTPUT");
+			// Following DRY to avoid duplication of code.
+			Comparator<Person> ascendingOrder = (person1, person2) -> person1.ageDifference(person2) ;
+			Comparator<Person> descendingOrder = (person1, person2) -> person2.ageDifference(person1) ;
+			
+			printPeople("Sorted in ascending order by age: ", people.stream().sorted(ascendingOrder).collect(Collectors.toList()));
+			printPeople("Sorted in descending order by age: ", people.stream().sorted(descendingOrder).collect(Collectors.toList()));
+			System.out.println("//" + "END:REVERSE_ORDER_OUTPUT");
+			
+			System.out.println("//" + "START:NAME_ASCEND_OUTPUT");
+			//Sorting by Name
+			printPeople("Sorted in ascending order by name: ", people.stream().sorted((person1, person2) -> person1.getName().compareTo(person2.getName())).collect(Collectors.toList()));
+			System.out.println("//" + "END:NAME_ASCEND_OUTPUT");
+		}
+		{
+			System.out.println("//" + "START:YOUNGEST_OUTPUT");
+			people.stream().min(Person::ageDifference).ifPresent(youngest -> System.out.println("Youngest:: "+ youngest));
+			System.out.println("//" + "END:YOUNGEST_OUTPUT");
+			
+			System.out.println("//" + "START:ELDEST_OUTPUT");
+			people.stream().max(Person::ageDifference).ifPresent(oldest -> System.out.println("eldest:: "+ oldest));
+			System.out.println("//" + "END:ELDEST_OUTPUT");
 		}
 		
 
