aa138f49daa631665241119386041f2979c24e2e
Akshaykumar <cruseakshay@gmail.com>
2019-01-18 14:56:05 +0530

    Using sorted() on stream with Comparator

--- JAVA/Functional Programming/StringComparatorAnFilter/Comparators/src/app/Compare.java
+++ JAVA/Functional Programming/StringComparatorAnFilter/Comparators/src/app/Compare.java
@@ -2,13 +2,24 @@ package app;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Compare {
+	public static void printPeople(final String message, final List<Person> people) {
+
+		System.out.println(message);
+		people.forEach(System.out::println);
+	}
+
 	public static void main(String[] args) {
-		final List<Person> people = Arrays.asList(new Person("John", 20), 
-												new Person("Sara", 21),
-												new Person("Jane", 21), 
-												new Person("Greg", 35));
-		
+		final List<Person> people = Arrays.asList(new Person("John", 20), new Person("Sara", 21),
+				new Person("Jane", 21), new Person("Greg", 35));
+		/*
+		 * sorted() accepts Comparator Functional Interface for Sorting the objects from stream.
+		 */
+		final List<Person> ascendingAge = people.stream().sorted((person1, person2) -> person1.ageDifference(person2))
+				.collect(Collectors.toList());
+		printPeople("Sorted in ascending order by age:", ascendingAge);
+
 	}
 }
