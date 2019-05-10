4b745a9c94711d6797c3a600b5dfdfe2b442998b
Akshaykumar Salunke <cruseakshay@gmail.com>
2019-05-10 10:33:33 +0530

    National Elections simulations

--- ML/python/Statistical Simulation in Python/code.py
+++ ML/python/Statistical Simulation in Python/code.py
@@ -98,3 +98,24 @@ while 1:
 answer = lottery_ticket_cost - 1
 
 print("The highest price at which it makes sense to buy the ticket is {}".format(answer))
+
+# Problem Statement:
+# National elections
+# This exercise will give you a taste of how you can model a DGP at different levels of complexity.
+
+# Consider national elections in a country with two political parties - Red and Blue. This country has 50 states and the party that wins the most states wins the elections. 
+# You have the probability p of Red winning in each individual state and want to know the probability of Red winning nationally.
+
+# Let's model the DGP to understand the distribution. Suppose the election outcome in each state follows a binomial distribution with probability p such that 0 indicates a loss for Red and 1 indicates a win. 
+# We then simulate a number of election outcomes. Finally, we can ask rich questions like what is the probability of Red winning less than 45% of the states?
+outcomes, sims, probs = [], 1000, p
+
+for _ in range(sims):
+    # Simulate elections in the 50 states
+    election = np.random.binomial(p = probs, n = 1)
+    # Get average of Red wins and add to `outcomes`
+    outcomes.append(election.mean())
+
+# Calculate probability of Red winning in less than 45% of the states
+prob_red_wins = sum([(x < 0.45) for x in outcomes]) / len(outcomes)
+print("Probability of Red winning in less than 45% of the states = {}".format(prob_red_wins))
