e3e9ddb07b21c031d75bd783d1a7160245db6645
Akshaykumar Salunke <cruseakshay@gmail.com>
2019-05-24 08:22:48 +0530

    Modeling Profits

--- ML/python/Statistical Simulation in Python/code.py
+++ ML/python/Statistical Simulation in Python/code.py
@@ -399,3 +399,26 @@ def corn_produced(rain, cost):
 # Simulate and print corn production
 corn_result = corn_produced(rain, cost)
 print("Simulated Corn Production = {}".format(corn_result))
+
+# Modeling Profits
+# In the previous exercise, you built a model of corn production. For a small farm, you typically have no control over the price or demand for corn. 
+# Suppose that price is normally distributed with mean 40 and standard deviation 10. You are given a function corn_demanded(), which takes the price and determines the demand for corn. 
+# This is reasonable because demand is usually determined by the market and is not in your control.
+
+# In this exercise, you will work on a function to calculate the profit by pulling together all the other simulated variables. The only input to this function 
+# will be the cost. Upon completion, you will have a function that will give you one simulated profit outcome for a given cost. This function can then be used for planning your costs.
+# Function to calculate profits
+def profits(cost):
+    rain = np.random.normal(50, 15)
+    price = np.random.normal(40, 10)
+    supply = corn_produced(rain, cost)
+    demand = corn_demanded(price)
+    equil_short = supply <= demand
+    if equil_short ==True:
+        tmp = supply*price - cost
+        return tmp
+    else: 
+        tmp2 = demand*price - cost
+        return tmp2
+result = profits(cost)
+print("Simulated profit = {}".format(result))
