72a88b74b852ce60f032f5aa2241f84e5faffccd
Akshaykumar Salunke <cruseakshay@gmail.com>
2019-09-05 23:19:47 +0530

    Calculating the value of pi

--- ML/python/Statistical Simulation in Python/code.py
+++ ML/python/Statistical Simulation in Python/code.py
@@ -452,3 +452,19 @@ def sim_integrate(func, xmin, xmax, sims):
 # Call the sim_integrate function and print results
 result = sim_integrate(func = lambda x: x*np.exp(x), xmin = 0, xmax = 1, sims = 50)
 print("Simulated answer = {}, Actual Answer = 1".format(result))
+
+# Calculating the value of pi
+# Initialize sims and circle_points
+sims, circle_points = 10000, 0 
+
+for i in range(sims):
+    # Generate the two coordinates of a point
+    point = np.random.uniform(-1,1,size=2)
+    # if the point lies within the unit circle, increment counter
+    within_circle = point[0]**2 + point[1]**2 <= 1
+    if within_circle == True:
+        circle_points +=1
+        
+# Estimate pi as 4 times the avg number of points in the circle.
+pi_sim = 4 * circle_points/sims
+print("Simulated value of pi = {}".format(pi_sim))
