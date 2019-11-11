e229168c39704788d7c116575254da30c4f0ab3e
Akshaykumar Salunke <cruseakshay@gmail.com>
2019-11-11 19:29:31 +0530

    find a duplicate qusetion
    for interview prep

---
+++ JAVA/findTheDuplicateProblem.java
@@ -0,0 +1,78 @@
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import static org.junit.Assert.*;
+
+public class Solution {
+
+    public static int findRepeat(int[] numbers) {
+
+        // find a number that appears more than once
+        
+
+        return 0;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // tests
+
+    @Test
+    public void justTheRepeatedNumberTest() {
+        final int[] numbers = {1, 1};
+        final int expected = 1;
+        final int actual = findRepeat(numbers);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shortArrayTest() {
+        final int[] numbers = {1, 2, 3, 2};
+        final int expected = 2;
+        final int actual = findRepeat(numbers);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void mediumArrayTest() {
+        final int[] numbers = {1, 2, 5, 5, 5, 5};
+        final int expected = 5;
+        final int actual = findRepeat(numbers);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void longArrayTest() {
+        final int[] numbers = {4, 1, 4, 8, 3, 2, 7, 6, 5};
+        final int expected = 4;
+        final int actual = findRepeat(numbers);
+        assertEquals(expected, actual);
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Solution.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
+}
